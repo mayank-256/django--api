944cf2b078fe4c6f3704ec3b394f7eb3d6966633
mayank-256 <mayank.yadav@thoughtwin.com>
2026-06-08 16:35:43 +0530

    Added time api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from datetime import datetime
 
-# Create your views here.
+def current_time(request):
+    return JsonResponse({
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
